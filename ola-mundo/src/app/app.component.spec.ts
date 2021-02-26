2efc8cbe54142e9974962086d15d2ea361d7a5b6
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-02-26 19:25:46 -0300

    Fix e2e and spec test

--- ola-mundo/src/app/app.component.spec.ts
+++ ola-mundo/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Olá mundo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Olá mundo!');
   });
 });
