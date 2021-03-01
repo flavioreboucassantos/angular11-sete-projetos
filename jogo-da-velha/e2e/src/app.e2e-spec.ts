25262c238fe526c1be27460f01323b4c01b2c691
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-02-28 22:50:45 -0300

    Finishes jogo-da-velha project

---
+++ jogo-da-velha/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,11 @@
+import { browser, logging } from 'protractor';
+import { AppPage } from './app.po';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+  
+});
